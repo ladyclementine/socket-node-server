9c35a8fce9ce21f897164b3e40bf24cac0dfed76
ladyclementine <rayanesantos-131@hotmail.com>
2018-07-27 17:57:15 -0300

    tentando encontrar chave unica para uso

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+
+    function AlfaNumerica(){ 
+        var letras = '0123456789abcdefghijklmnopqrstuvwxyz'; 
+        var aleatorio = ''; 
+        var tamanho=10; 
+        for (var i = 0; i < tamanho; i++) { 
+        var rnum = Math.floor(Math.random() * letras.length); 
+        aleatorio += letras.substring(rnum, rnum + 1); 
+        } 
+        return aleatorio
+    }
     // handle incoming connections from clients
     io.sockets.on('connection', function(socket) {
         // console.log(io.sockets.connected)
        // join then specific room
         socket.on('room', function(room){
-            io.emit('callSpecificUser', {user_room:room.user_room, user_host_room:room.user_host_room, room_id:socket.id})
+            var room_id =  socket.id
+            console.log(AlfaNumerica())
+            socket.join(AlfaNumerica())
+            io.emit('callSpecificUser', {user_room:room.user_room, user_host_room:room.user_host_room, room_id:room_id})
         });
         socket.on('Enterroom', (room) => {
+            console.log('enter room', room)
             socket.join(room)
         })
         socket.on('add-message', (message) => {
-            console.log('message para', message.room_info.user_host_room)
-            io.in(message.room_info.user_host_room).emit('message',  {text: message.text, from: message.current_user, created: new Date()});
+            console.log('add-message', message.room_info.room_id)
+            io.in(message.room_info.room_id).emit('message',  {text: message.text, from: message.current_user, created: new Date()});
         });
         socket.on('disconnect', function(){
             io.emit('users-changed', {user: socket.nickname, event: 'left'});   
