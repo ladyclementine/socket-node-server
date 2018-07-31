4fee861c7e2c7e9fa884a46148a320bf2e0578aa
ladyclementine <rayanesantos-131@hotmail.com>
2018-07-31 17:25:25 -0300

    updating

--- index.js
+++ index.js
@@ -16,18 +16,23 @@ let io = require('socket.io')(http);
     io.sockets.on('connection', function(socket) {
        // join then specific room
         socket.on('room', function(room){
-            var room_id =  socket.id
+            var room_id =  room.room_id
             socket.join(room_id)
             console.log('id do canal', room.user_room + room.user_host_room, room_id)
             io.emit('callSpecificUser', {user_room:room.user_room, user_host_room:room.user_host_room, room_id:room_id})
         });
         socket.on('Enterroom', (room) => {
+            io.in(room).clients((err , clients) => {
+                console.log(clients)
+                // clients will be array of socket ids , currently available in given room
+            });
             console.log('Join no canal de id', room)
             socket.join(room)
         })
         socket.on('add-message', (message) => {
             console.log('messagem de', message.current_user, 'no canal id', message.room_info.room_id)
-            io.in(message.room_info.room_id).emit('message',  {text: message.text, from: message.current_user, created: new Date()});
+            console.log('mandando', message.text, 'para room', message.room_info.room_id )
+            io.to(message.room_info.room_id).emit('message',  {text: message.text, from: message.current_user, created: new Date()});
         });
         socket.on('disconnect', function(){
             io.emit('users-changed', {user: socket.nickname, event: 'left'});   
